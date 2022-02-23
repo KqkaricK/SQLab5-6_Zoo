b9c62b4f78c8705715c4387661e11f6e72d0f3ba
Kozurev <Kozurev@K-PC>
2022-02-23 11:55:44 +0700

    Добавьте файлы проекта.

---
+++ Zoo/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Zoo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 11);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 53;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(476, 185);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(11, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выбрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(412, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(475, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "1. Список 5 самых дорогих зоопарков";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(475, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "2. Список 5 самых больших по численности животных зоопарков";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 291);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(475, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "3. Список 5 самых больших по численности видов животных зоопарков";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(11, 320);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(475, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "4. Список зоопарков с указанием количества животных в них";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(11, 349);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(475, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "5. Список зоопарков с указанием количества видов животных в них";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(307, 203);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(97, 21);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Удалить строку";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 386);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(50, 402);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(105, 13);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "github.com/kqkarick";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Created and Development by АК";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 426);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "SQLab5-6_Zoo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Zoo/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace Zoo
+{
+    public partial class Form1 : Form
+    {
+
+        OleDbConnection cn;
+        DataTable tbls;
+        OleDbDataAdapter dbAdapter1;
+        DataTable tb;
+        OleDbCommandBuilder cb;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Directory.GetCurrentDirectory() + @"\Зоопарк.accdb");
+            cn.Open();
+            tbls = cn.GetSchema("Tables", new string[] { null, null, null, "TABLE" }); //список всех таблиц
+            foreach (DataRow row in tbls.Rows)
+            {
+                string TableName = row["TABLE_NAME"].ToString();
+                comboBox1.Items.Add(TableName);
+            };
+
+        }
+
+        void button2_change(bool a)
+        {
+            if (a == true) { button2.Enabled = true; button8.Enabled = true; }
+            else { button2.Enabled = false; button8.Enabled = false; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2_change(true);
+            if (comboBox1.SelectedIndex != -1)
+            {
+                dbAdapter1 = new OleDbDataAdapter(@"SELECT * FROM " + comboBox1.SelectedItem, cn);
+                tb = new DataTable();
+                dbAdapter1.Fill(tb);
+                dataGridView1.DataSource = tb;
+            }
+            else
+            {
+                MessageBox.Show("Не выбрана таблица для запроса");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cb = new OleDbCommandBuilder(dbAdapter1);
+            dbAdapter1.Update(tb);
+            MessageBox.Show("Сохраненно");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2_change(false);
+            dbAdapter1 = new OleDbDataAdapter(@"SELECT TOP 5 Зоопарк.СтоимостьБилета, Зоопарк.НазваниеЗоопарка FROM Зоопарк ORDER BY Зоопарк.СтоимостьБилета DESC;", cn);
+            tb = new DataTable();
+            dbAdapter1.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button2_change(false);
+            dbAdapter1 = new OleDbDataAdapter(@"
+            SELECT TOP 5 Sum(Наличие.Количество) AS Количество, A.НазваниеЗоопарка
+            FROM (SELECT Зоопарк.КодЗоопарка, Зоопарк.НазваниеЗоопарка, Наличие.Количество
+            FROM Зоопарк INNER JOIN (Животное INNER JOIN Наличие ON Животное.КодЖивотного = Наличие.КодЖивотного) ON Зоопарк.КодЗоопарка = Наличие.КодЗоопарка)  AS A
+            GROUP BY A.НазваниеЗоопарка, A.Зоопарк.КодЗоопарка
+            ORDER BY Sum(Наличие.Количество) DESC;"
+            , cn);
+            tb = new DataTable();
+            dbAdapter1.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button2_change(false);
+            dbAdapter1 = new OleDbDataAdapter(@"
+            SELECT TOP 5 COUNT(Наличие.Количество) AS Количество, [A].НазваниеЗоопарка
+            FROM (SELECT Зоопарк.КодЗоопарка, Зоопарк.НазваниеЗоопарка, Наличие.Количество
+            FROM Зоопарк INNER JOIN (Животное INNER JOIN Наличие ON Животное.КодЖивотного = Наличие.КодЖивотного) ON Зоопарк.КодЗоопарка = Наличие.КодЗоопарка)  AS [A]
+            GROUP BY [A].Зоопарк.КодЗоопарка, [A].НазваниеЗоопарка
+            ORDER BY COUNT(Наличие.Количество) DESC;"
+            , cn);
+            tb = new DataTable();
+            dbAdapter1.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button2_change(false);
+            dbAdapter1 = new OleDbDataAdapter(@"
+            SELECT Наличие.Количество, Животное.НазваниеЖивотного, Зоопарк.НазваниеЗоопарка
+            FROM Животное INNER JOIN (Зоопарк INNER JOIN Наличие ON Зоопарк.КодЗоопарка = Наличие.КодЗоопарка) ON Животное.КодЖивотного = Наличие.КодЖивотного
+            ORDER BY Зоопарк.НазваниеЗоопарка;"
+            , cn);
+            tb = new DataTable();
+            dbAdapter1.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button2_change(false);
+            dbAdapter1 = new OleDbDataAdapter(@"
+            SELECT COUNT(Наличие.Количество) AS Количество, [A].НазваниеЗоопарка
+            FROM (SELECT Зоопарк.КодЗоопарка, Зоопарк.НазваниеЗоопарка, Наличие.Количество
+            FROM Зоопарк INNER JOIN (Животное INNER JOIN Наличие ON Животное.КодЖивотного = Наличие.КодЖивотного) ON Зоопарк.КодЗоопарка = Наличие.КодЗоопарка)  AS [A]
+            GROUP BY [A].Зоопарк.КодЗоопарка, [A].НазваниеЗоопарка"
+            , cn);
+            tb = new DataTable();
+            dbAdapter1.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(rowIndex);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/kqkarick");
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Cursor = Cursors.Hand;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Cursor = Cursors.Default;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/kqkarick");
+        }
+    }
+}
